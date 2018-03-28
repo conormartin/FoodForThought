4cff3ec1ac767c66021182e2bd378b38ec13c965
Conor Martin <conorarranmore@gmail.com>
2018-03-28 20:38:34 +0100

    updated food log page

--- app/app.js
+++ app/app.js
@@ -7,11 +7,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+
+
+//default homepage route
 app.get("/", function(req, res){
     res.render("home");
 });
 
-// Runs this function when user clicks on SearchResults page
+
+
+
+// get request to API when user searches for food in navbar
 app.get("/search", function(req, res) {
     var searchTerm = req.query.searchTerm;
 
@@ -33,8 +39,12 @@ app.get("/search", function(req, res) {
     );
 });
 
-// Runs this function when user clicks on Nutrients page
-app.get("/Nutrients/", function(req, res) {
+
+
+
+// on /search results page, user selects which food they want, quantity and measurement, this is sent in json as a post request
+///nutrients page displays nutritional breakdown of the selected food
+app.get("/Nutrients", function(req, res) {
     //json object containing info on food type & quantity
     var quantity = req.query.quantity.replace(/['"]+/g, '');
     var measurement = req.query.measurement.replace(/['"]+/g, '');
@@ -69,21 +79,36 @@ app.get("/Nutrients/", function(req, res) {
     })
 });
 
+
+
+
+
+//renders blog page
 app.get("/blogPage", function(req, res) {
     res.render("blogPage");
 });
 
-// arrays to save temporarily values until firebase is working
+
+
+
+
+
+// arrays to temporarily save food log values until firebase is working
 var foodType = [];
 var measurement = [];
 var loggedFood = [];
+var loggedQuantity = [];
 
+//shows basic foodlog page when clicked on navbar
 app.get("/foodlog", function(req,res) {
     res.render("foodlog", {foodType:foodType, measures:measurement, loggedFood:loggedFood}); 
-    foodType = [];
-    measurement = [];
 })
 
+
+
+
+
+//shows foodlog page with search results
 app.post("/foodlog", function(req,res) {
     var searchTerm = req.body.searchTerm;
     request({
@@ -101,32 +126,70 @@ app.post("/foodlog", function(req,res) {
     });
 });
 
+
+
+
+
+//shows foodlog page with data submitted to log (not finished)
 app.get("/foodlog:submitted", function(req,res) {
-    var quantity = req.query.quantity;
-    var measure = req.query.measurement;
-    var food = req.query.foodType;
+    var quantity = req.query.quantity.replace(/['"]+/g, '');
+    var measure = req.query.measurement.replace(/['"]+/g, '');
+    var food = req.query.foodType.replace(/['"]+/g, '');
+    var foodUrl = req.query.foodUrl.replace(/['"]+/g, '');
+    var measurementUrl = req.query.measurement.replace(/['"]+/g, '');
     
-    loggedFood.push(food + " - " + quantity + measure);
-    
-    res.render("foodlog", {foodType: foodType, measures: measurement, loggedFood:loggedFood});
+    var foodJson = {
+        "yield": 1,
+        "ingredients": [{
+            "quantity": Number(quantity),
+            "measureURI": measurementUrl,
+            "foodURI": foodUrl
+        }]
+    };
+    request({
+        url: "https://api.edamam.com/api/food-database/nutrients?app_id=2833224e&app_key=f49370b187bbd05209a7472b217a70d7",
+        method: "POST",
+        json: true,
+        body: foodJson
+    }, function (error, response, body){
+        var nutrients = body.totalNutrients;
+    });
+        
+    loggedFood.push(food);
+    loggedQuantity.push(quantity);
+    res.render("foodlog", {foodType:foodType, measures:measurement, loggedFood:loggedFood, loggedQuantity:loggedQuantity});
 });
 
+
+
+//renders diet breakdown table from foodlog page (just a template for now)
 app.get("/dietbreakdown", function(req, res){
     res.render("dietbreakdown");
 });
 
+
+
+//shows contact page (empty)
 app.get("/ContactUs", function(req, res){
     res.render("contact");
 });
 
+
+
+//shows about page (empty)
 app.get("/about", function(req, res){
     res.render("about");
 });
 
-// Reroutes all other requests to a default error message
+
+
+
+// Reroutes all other requests to a default error message (eg. page does not exist)
 app.get("*", function(req, res) {
     res.render("errorPage");
 });
+
+
 
 // Activates server on port 3000
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
